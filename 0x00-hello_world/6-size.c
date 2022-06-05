59a9913016a6ff62d0b86c77b8f189608c6e50c0
Nnastein <nnannamelaugha@gmail.com>
2022-06-04 20:13:11 -0700

    I just dey dub

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Size of 
+ *
+ * Return: 0 Always (Success)
+ */
+
+int main(void)
+{
+	printf("Size of a char: 1 byte(s)\n
+		Size of an int: 4 byte(s)\n
+		Size of a long int: 4 byte(s)\n
+		Size of a long long int: 8 byte(s)\n
+		Size of a float: 4 byte(s)")
+	return (0);
+}
