149836d71942825283058b850d0fc63ef41b7cd4
Nnastein <nnannamelaugha@gmail.com>
2022-06-08 21:06:13 -0700

    last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,16 +9,14 @@
 
 int print_last_digit(int n)
 {
-	if (n > 0)
+	int last_digit;
+
+	last_digit = n % 10;
+	if (n < 0)
 	{
-		return (n % 10);
+		last_digit = last_digit * -1;
 	}
-	else if (n < 0)
-	{
-		return (-n % 10);
-	}
-	else
-	{
-		return (n);
-	}
+	_putchar(last_digit + '0');
+
+	return (last_digit);
 }
