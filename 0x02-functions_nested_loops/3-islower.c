4c5653481fdcc0afdc8a4a6c19fc6749e325cdcc
Nnastein <nnannamelaugha@gmail.com>
2022-06-08 19:53:01 -0700

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * _islower(int c) - checks lowercase
+ *
+ * Description - Wo
+ *
+ * Return: int
+ */
+
+int _islower(int c)
+{
+	if (islower(c) == True)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
