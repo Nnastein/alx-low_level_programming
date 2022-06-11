6fa5d4904649a2dea9e68848d334eb4500e096d3
Nnastein <nnannamelaugha@gmail.com>
2022-06-10 19:21:51 -0700

    check letter case

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isupper(c)) - whatever
+ *
+ * Return: 0 or 1
+ */
+
+int _isupper(int c)
+{
+	if (c > 64 && c < 91)
+	{
+		return (1);
+	}
+	else if (c > 96 && c < 123)
+	{
+		return (0);
+	}
+}
