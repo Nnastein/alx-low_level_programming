37396540d0105a5017c96c763030b17529e644a0
Nnastein <nnannamelaugha@gmail.com>
2022-06-10 20:07:45 -0700

    Multiplication

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -2,13 +2,16 @@
 
 /**
  * mul - multiplies two ints
- * @a - first number
- * @b - second number
+ * @a: first number
+ * @b: second number
  *
  * Return: a * b
  */
 
 int mul(int a, int b)
 {
-	return (a * b);
+	int ans;
+
+	ans = a * b;
+	return (ans);
 }
