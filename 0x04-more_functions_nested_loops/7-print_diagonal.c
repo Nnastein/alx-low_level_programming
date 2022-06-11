1a21b0bde91139885a0ac8a42c25ba189eb904a1
Nnastein <nnannamelaugha@gmail.com>
2022-06-11 14:03:21 -0700

    diagonal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -17,17 +17,20 @@ void print_diagonal(int n)
 		b = 0;
 		while (b < n)
 		{
-			_putchar(92);
-			_putchar(10);
-			if (b < (n-1))
-			{
+			/*_putchar(92);*/
+			/*_putchar(10);*/
+			/*if (b < (n-1))*/
+			/*{*/
 				a = 0;
-				while (a < (b+1))
+				/*while (a < (b+1))*/
+				while (a < b)
 				{
 					_putchar(32);
 					a++;
 				}
-			}
+				_putchar(92);
+				_putchar(10);
+			/*}*/
 			b++;
 		}
 	}
