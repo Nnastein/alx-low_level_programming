aa7f2330f141ba9351cf511fa2a56c533f3e3643
Nnastein <nnannamelaugha@gmail.com>
2022-06-10 19:41:28 -0700

    check letter case

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,8 +1,9 @@
 #include "main.h"
 
 /**
- * _isupper(c)) - whatever
- *
+ * _isupper - whatever
+ * @c : checks the case
+ * 
  * Return: 0 or 1
  */
 
@@ -10,11 +11,10 @@ int _isupper(int c)
 {
 	if (c > 64 && c < 91)
 	{
-		c = 1;
+		return (1);
 	}
 	else if (c > 96 && c < 123)
 	{
-		c = 0;
+		return (0);
 	}
-	return (c);
 }
