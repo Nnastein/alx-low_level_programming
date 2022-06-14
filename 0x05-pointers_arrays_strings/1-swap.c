4bba7d5778e79a1740962e55be9107aad10ba541
Nnastein <nnannamelaugha@gmail.com>
2022-06-13 21:43:10 -0700

    Pointers and Arrays

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * swap_int - swaps ints
+ * @a: one kain variable
+ * @b: another kain var
+ *
+ * Return: 0
+ */
+
+void swap_int(int *a, int *b);
+{
+	int temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp
+}
