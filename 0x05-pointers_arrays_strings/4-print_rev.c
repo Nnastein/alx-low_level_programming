58d975947cd41d6ad22c8949e3ce32acafeb39a2
Nnastein <nnannamelaugha@gmail.com>
2022-06-14 19:01:55 -0700

    Pointers & Arrays

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * print_rev - print string in reverse
+ * @s: char s
+ *
+ * Return: 0
+ */
+
+void _print_rev(char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	i--;
+	while (i >= 0)
+	{
+		_putchar(s[i]);
+		i--;
+	}
+	_putchar('\n');
+}
