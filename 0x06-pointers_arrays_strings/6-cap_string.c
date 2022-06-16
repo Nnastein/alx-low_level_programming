4a11a4ad548559dba47fe0ee5a16f891acfcf2b0
Nnastein <nnannamelaugha@gmail.com>
2022-06-15 23:27:45 -0700

    Omo

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * cap_string - Caps all words
+ * @s: string
+ *
+ * Return: char
+ */
+
+char *cap_string(char *)
+{
+	int i = 0;
+	int position;
+
+	while (s[i] != '\0')
+	{
+		if ((s[i] == ' ' || s[i] == '\n' || s[i] == '\t' || s[i] == '.' || (s[i] >= '0' && s[i] <= '9')) && (s[i+1] >= 'a' && s[i+1] <= 'z'))
+		{
+			position = s[i+1] - 'a';
+			s[i+1] = ('A' + position);
+		}
+		i++;
+	}
+	return (s);
+}
