6dadea5b253933ceed54b1d7d9c1229768b87c6c
Nnastein <nnannamelaugha@gmail.com>
2022-06-15 19:26:44 -0700

    Omo

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -15,12 +15,12 @@ char *_strncpy(char *dest, char *src, int n)
 	int j;
 
 	i = 0;
-	while (src[n] != '\0')
+	while (src[i] != '\0')
 	{
 		dest[i] = src[i];
-		num++;
+		i++;
 	}
-	for (i = 0 ; j < n; i++)
+	for (j = 0 ; j < n; j++)
 		dest[j] = '\0';
 
 	return (dest);
