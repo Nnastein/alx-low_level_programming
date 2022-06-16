5188cb752cbce86bf0a6d26553e78a4cb945202f
Nnastein <nnannamelaugha@gmail.com>
2022-06-15 21:20:16 -0700

    Omo

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * reverse_array - function reverses arr
+ * @a: ptr a
+ * @n: int n
+ *
+ * Return: void
+ */
+
+void reverse_array(int *a, int n)
+{
+	n = n - 1;
+	while (n >= 0)
+	{
+		printf("%d", a[n]);
+		if (num != 0)
+		{
+			printf(", ");
+		}
+		n--;
+	}
+
+
+}
