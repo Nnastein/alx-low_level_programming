8f7bf958d9b3e09c3413e210ba74953ebe49341c
Nnastein <nnannamelaugha@gmail.com>
2022-06-15 19:53:44 -0700

    Omo

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _strncmp - function that compares two strings
+ * @s1: first char
+ * @s2: second char
+ *
+ * Return: Integer
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+	int j;
+
+	i = 0;
+	j = 0;
+	while (s1[i] != '\0')
+	{
+		i++;
+	}
+	while (s2[j] != '\0')
+	{
+		j++;
+	}
+	if (i < j)
+		return (-15);
+	else if (i > j)
+		return (15);
+	else
+		return (0);
+}
