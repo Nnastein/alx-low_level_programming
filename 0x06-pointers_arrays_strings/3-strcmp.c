eb6bdb889d3d3af578291450339cfd8dc40ee63e
Nnastein <nnannamelaugha@gmail.com>
2022-06-15 20:23:28 -0700

    Omo

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -12,20 +12,11 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int i;
-	int j;
-
-	i = 0;
-	while (s1[i] != '\0')
-		i++;
-	j = 0;
-	while (s2[j] != '\0')
-		j++;
-
-	if (i < j)
-		return (-15);
-	else if (i > j)
-		return (15);
-	else
-		return (0);
+	while(*s1 && (*s1 == *s2))
+	{
+        	s1++;
+	        s2++;
+	}
+	
+	return (*s1 - *s2);
 }
