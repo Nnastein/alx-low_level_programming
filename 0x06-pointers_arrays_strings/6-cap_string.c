9642d4de4dd78c2071d552379c93a431bf68249c
Nnastein <nnannamelaugha@gmail.com>
2022-06-16 17:01:42 -0700

    Omo

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,17 +8,20 @@
  * Return: char
  */
 
-char *cap_string(char *)
+char *cap_string(char *s)
 {
 	int i = 0;
 	int position;
 
 	while (s[i] != '\0')
 	{
-		if ((s[i] == ' ' || s[i] == '\n' || s[i] == '\t' || s[i] == '.') && (s[i+1] >= 'a' && s[i+1] <= 'z'))
+		if ((s[i] == ',' || s[i] == '.' || s[i] == ' ' || s[i] == '\n' || s[i] == '\t' || s[i] == ';' || s[i] == '!' || s[i] == '?' || s[i] == '"' || s[i] == '(' || s[i] == ')' || s[i] == '{' || s[i] == '}'))
 		{
-			position = s[i+1] - 'a';
-			s[i+1] = ('A' + position);
+			if (s[i+1] >= 'a' && s[i+1] <= 'z')
+			{
+				position = s[i+1] - 'a';
+				s[i+1] = ('A' + position);
+			}
 		}
 		i++;
 	}
