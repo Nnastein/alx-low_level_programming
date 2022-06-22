fc1f6e048aa4f806c3c63a62e27de577a5360102
Nnastein <nnannamelaugha@gmail.com>
2022-06-21 21:20:36 -0700

    Even more pointers

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_chessboard - prints the chessboard
+ * @a: arg one kain
+ *
+ * Return: void
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int i;
+	int j;
+
+	i = 0;
+	while (i < *(a + i))
+	{
+		j = 0;
+		while (j < 8)
+		{
+			_putchar(board[i][j]);
+			if (j == 7)
+			{
+				_putchar('\n');
+			}
+			j++;
+		}
+		i++;
+	}
+}
