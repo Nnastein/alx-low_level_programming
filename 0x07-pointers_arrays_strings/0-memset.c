13b34604879967828f0880ce030c202c5322e86f
Nnastein <nnannamelaugha@gmail.com>
2022-06-21 19:17:40 -0700

    Even more pointers

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -4,8 +4,8 @@
 /**
  * _memset - fills memory with a constant byte
  * @s: the address of the memory to print
- * @c:
- * @n:
+ * @c: buffer address 0x01
+ * @n: the size of the memory to print
  *
  * Return: Char pointer s.
  */
