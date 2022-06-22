a8d8bcebefc064dd577e478fe98d0c51312ea61c
Nnastein <nnannamelaugha@gmail.com>
2022-06-21 21:24:51 -0700

    Even more pointers

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -14,12 +14,12 @@ void print_chessboard(char (*a)[8])
 	int j;
 
 	i = 0;
-	while (i < *(a + i))
+	while (i < 8)
 	{
 		j = 0;
 		while (j < 8)
 		{
-			_putchar(board[i][j]);
+			_putchar(a[i][j]);
 			if (j == 7)
 			{
 				_putchar('\n');
