3b7ac712eafd8c1c30874ade4703b42154b75980
Nnastein <nnannamelaugha@gmail.com>
2022-06-21 21:02:11 -0700

    Even more pointers

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -15,14 +15,14 @@ char *_strstr(char *haystack, char *needle)
 	int j;
 
 	i = 0;
-	while (*(s + i) != '\0')
+	while (*(haystack + i) != '\0')
 	{
 		j = 0;
-		while (*(accept + j) != '\0')
+		while (*(needle + j) != '\0')
 		{
-			if (*(s + i) == *(accept + 0))
+			if (*(haystack + i) == *(needle + 0))
 			{
-				return (s + i);
+				return (haystack + i);
 			}
 			j++;
 		}
