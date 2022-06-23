93e26b08da0d90b18866b7c19172c3daf4e86f41
Nnastein <nnannamelaugha@gmail.com>
2022-06-22 19:59:45 -0700

    Recursion

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -2,22 +2,6 @@
 #include <stdio.h>
 
 /**
- * somedigits - takes the sum of ints
- * @n: arg
- *
- * Return: int
- */
-
-int somedigit(int n)
-{
-	if (n == 0)
-	{
-		return (0);
-	}
-	return (n % 10) + somedigit(n / 10);
-}
-
-/**
  * _strlen_recursion - returns the length of a string.
  * @s: array of char
  *
@@ -28,8 +12,7 @@ int _strlen_recursion(char *s)
 {
 	if (*s != '\0')
 	{
-		_strlen_recursion(s + 1);
-		somedigit(s + 1);
+		return (1 + _strlen_recursion(s + 1));
 	}
 	else
 	{
