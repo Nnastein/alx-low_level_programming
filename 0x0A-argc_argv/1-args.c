dc5a49bbc5feaf0beb7259814cdb59b61179cca0
Nnastein <nnannamelaugha@gmail.com>
2022-06-29 18:08:15 -0700

    Argc Argv

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints num of args passed into it
+ * @argc: num of args
+ * @argv: arr pf args
+ *
+ * Return: (0)
+ */
+
+int main (int argc, __attribute__((unused)) char *argv[])
+{
+	int args;
+
+	args = argc - 1;
+	printf("%d\n", args);
+	return (0);
+}
---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints product of two nums
+ * @argc: num of args
+ * @argv: arr pf args
+ *
+ * Return: (0)
+ */
+
+int main (int argc, char *argv[])
+{
+	if (argc <= 2 || argc > 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+		return (0);
+	}
+}
