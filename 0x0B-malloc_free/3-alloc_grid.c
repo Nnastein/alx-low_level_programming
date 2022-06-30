c68a37a2cf54e9801ccc9ecc96158f7bb9f2ce14
Nnastein <nnannamelaugha@gmail.com>
2022-06-29 17:29:55 -0700

    Mallocs and Free

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * alloc_grid - returns a pointer to a 2D array of integers
@@ -14,7 +15,7 @@ int **alloc_grid(int width, int height)
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
-	grid = (int **)malloc(height * sizeof(int *));
+	grid = (int **) malloc(height * sizeof(int *));
 
 	if (grid == NULL)
 		return (NULL);
