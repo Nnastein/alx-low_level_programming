37dc510ce3902c2fe72bb9872c6c42ec0b53c1f3
Nnastein <nnannamelaugha@gmail.com>
2022-07-08 20:51:54 -0700

    Functions Pointers

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -5,12 +5,13 @@
  * print_name - prints a name
  * @name: string
  * @f: function pointer
+ *
  * Return: void
  */
 
 void print_name(char *name, void (*f)(char *))
 {
 	if (f == NULL)
-	return;
+		return;
 	f(name);
 }
